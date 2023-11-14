00c2f826d496fc402426c8b415871ebf3d950894
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-11-14 10:33:47 +0200

    Add image source to Men's T-shirt and Sweaters sections

--- e-commerce/src/components/MenSweaters.jsx
+++ e-commerce/src/components/MenSweaters.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { SingleProduct } from './SingleProduct'
 
-export const MenSweaters = () => {
-  return (
-    <div className='bg-gray-200 flex flex-wrap justify-center items-center gap-5 px-8 py-10'>
+import image from '../images/product3.jpg';
 
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-    </div>
-  )
+export const MenSweaters = () => {
+    return (
+        <div className='bg-gray-200 flex flex-wrap justify-center items-center gap-5 px-8 py-10'>
+
+            <SingleProduct imageSource={image} />
+            <SingleProduct imageSource={image} />
+            <SingleProduct imageSource={image} />
+        </div>
+    )
 }
--- e-commerce/src/components/MenTshirts.jsx
+++ e-commerce/src/components/MenTshirts.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { SingleProduct } from './SingleProduct'
 
+import image from '../images/product3.jpg';
+
 export const MenTshirts = () => {
   return (
     <div className='bg-gray-200 flex flex-wrap justify-center items-center gap-5 px-8 py-10'>
 
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
+        <SingleProduct imageSource={image} />
     </div>
   )
 }
