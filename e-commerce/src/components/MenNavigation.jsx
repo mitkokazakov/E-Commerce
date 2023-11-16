edc1a79157ae4768459d94cd36e6457d3e241cda
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-11-16 11:42:39 +0200

    Make Men's navigation responsive

--- e-commerce/src/components/MenNavigation.jsx
+++ e-commerce/src/components/MenNavigation.jsx
@@ -10,7 +10,7 @@ export const MenNavigation = () => {
     return (
 
         <div className='bg-gray-200 flex flex-col mt-[100px]'>
-            <div className="flex justify-center items-center gap-16 px-5 py-16">
+            <div className="flex flex-col justify-center items-center gap-5 px-5 py-8 sm:flex-row sm:gap-16 sm:py-16">
                 <Link to="/men/tshirts" className=' text-xl font-bold text-teal-600 tracking-widest hover:scale-105 duration-200' state="T - Shirts">T - Shirts</Link>
                 <Link to="/men/sweaters" className=' text-xl font-bold text-teal-600 tracking-widest hover:scale-105 duration-200' state="Sweaters">Sweaters</Link>
                 <Link className=' text-xl font-bold text-teal-600 tracking-widest hover:scale-105 duration-200' state="Jeans">Jeans</Link>
@@ -22,6 +22,7 @@ export const MenNavigation = () => {
             </div>
             <Outlet />
         </div>
+
 
     )
 }
