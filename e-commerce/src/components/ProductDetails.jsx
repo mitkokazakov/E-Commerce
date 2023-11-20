bf7d37f16024b3a85afa04c4201bf6468da85db0
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-11-20 14:40:11 +0200

    Styles are changed in ProductDetails

--- e-commerce/src/components/ProductDetails.jsx
+++ e-commerce/src/components/ProductDetails.jsx
@@ -4,7 +4,7 @@ import image from '../images/product3.jpg';
 
 export const ProductDetails = () => {
     return (
-        <div className='flex flex-col gap-5 px-4'>
+        <div className='flex flex-col gap-5 px-4 py-6 md:flex-row md:gap-6'>
 
             <div className='flex flex-col gap-2'>
 
@@ -37,32 +37,34 @@ export const ProductDetails = () => {
 
             </div>
 
-            <p className='font-bold tracking-widest'>Home / T-Shirts</p>
-
-            <h1 className='text-xl font-bold tracking-widest'>Cartoon Astronaut T-Shirt</h1>
-
-            <h1 className='text-2xl font-extrabold tracking-widest'>$ 139.00</h1>
-
             <div className='flex flex-col gap-5'>
-                <select className='border-[1px] border-teal-500 text-center py-2 px-4 w-2/5'>
-                    <option value="0">Select Size</option>
-                    <option value="0">Small</option>
-                    <option value="0">Medium</option>
-                    <option value="0">Large</option>
-                    <option value="0">Extra Large</option>
-                </select>
+                <p className='font-bold tracking-widest'>Home / T-Shirts</p>
 
-                <div className='flex gap-5'>
-                    <input  className='border-[1px] border-teal-500 text-center py-2 px-4 w-1/6' type="number" name="" id="" />
+                <h1 className='text-xl font-bold tracking-widest'>Cartoon Astronaut T-Shirt</h1>
 
-                    <button className='bg-teal-600 text-white tracking-widest font-bold px-3 py-2 rounded-md'>Add to Cart</button>
+                <h1 className='text-2xl font-extrabold tracking-widest'>$ 139.00</h1>
+
+                <div className='flex flex-col gap-5'>
+                    <select className='border-[1px] border-teal-500 text-center py-2 px-4 w-2/5'>
+                        <option className='text-center' value="0">Select Size</option>
+                        <option value="0">Small</option>
+                        <option value="0">Medium</option>
+                        <option value="0">Large</option>
+                        <option value="0">Extra Large</option>
+                    </select>
+
+                    <div className='flex gap-5'>
+                        <input className='border-[1px] border-teal-500 text-center py-2 px-4 w-1/6 md:w-1/4 lg:w-1/6' type="number" name="" id="" />
+
+                        <button className='bg-teal-600 text-white tracking-widest font-bold px-3 py-2 rounded-m '>Add to Cart</button>
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <h3 className='font-bold text-xl tracking-widest mb-5'>Product Details</h3>
+                <div>
+                    <h3 className='font-bold text-xl tracking-widest mb-5'>Product Details</h3>
 
-                <p className='font-light text-slate-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque velit numquam dolor, nulla quisquam quam, omnis dolorum non earum tenetur quia deleniti repellendus dolorem debitis veniam consequuntur iure optio nesciunt. Sed quibusdam eaque, ratione dignissimos adipisci aspernatur maxime. Expedita, qui. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque velit numquam dolor, nulla quisquam quam, omnis dolorum non earum tenetur quia deleniti repellendus dolorem debitis veniam consequuntur iure optio nesciunt. Sed quibusdam eaque, ratione dignissimos adipisci aspernatur maxime. Expedita, qui.</p>
+                    <p className='font-light text-slate-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque velit numquam dolor, nulla quisquam quam, omnis dolorum non earum tenetur quia deleniti repellendus dolorem debitis veniam consequuntur iure optio nesciunt. Sed quibusdam eaque, ratione dignissimos adipisci aspernatur maxime. Expedita, qui. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque velit numquam dolor, nulla quisquam quam, omnis dolorum non earum tenetur quia deleniti repellendus dolorem debitis veniam consequuntur iure optio nesciunt. Sed quibusdam eaque, ratione dignissimos adipisci aspernatur maxime. Expedita, qui.</p>
+                </div>
             </div>
         </div>
     )
