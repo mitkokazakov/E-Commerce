beddbd444cf2a81c4ee23039a074efebe7977074
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-11-20 10:08:54 +0200

    Shopping cart styles changed

--- e-commerce/src/components/ShoppingCart.jsx
+++ e-commerce/src/components/ShoppingCart.jsx
@@ -4,14 +4,14 @@ import image from '../images/product5.jpg';
 
 export const ShoppingCart = () => {
     return (
-        <div className='flex flex-col py-6 bg-slate-300 px-3 gap-4'>
+        <div className='flex flex-col py-6 bg-slate-100 px-3 gap-4 md:px-20 sm:px-8'>
             <h1 className='text-center text-2xl font-bold tracking-widest mb-10'>My Cart</h1>
 
             <div className='flex flex-col gap-10 '>
 
                 <div className='flex gap-2  px-4 py-4 bg-gradient-to-b from-slate-100 to-slate-200 rounded-xl relative'>
 
-                    <div className=' w-full h-44 relative md:max-w-xs md:h-auto'>
+                    <div className=' w-full h-44 relative md:max-w-xs md:h-auto sm:w-1/3'>
                         <img className='w-full h-full rounded-2xl' src={image} alt="" />
 
                         <div className='w-full h-full z-10 top-0 bg-gradient-to-b from-slate-200 to-slate-400 opacity-10 absolute rounded-2xl'></div>
@@ -36,7 +36,7 @@ export const ShoppingCart = () => {
 
                 <div className='flex gap-2  px-4 py-4 bg-gradient-to-b from-slate-100 to-slate-200 rounded-xl relative'>
 
-                    <div className=' w-full h-44 relative md:max-w-xs md:h-auto'>
+                    <div className=' w-full h-44 relative md:max-w-xs md:h-auto sm:w-1/3'>
                         <img className='w-full h-full rounded-2xl' src={image} alt="" />
 
                         <div className='w-full h-full z-10 top-0 bg-gradient-to-b from-slate-200 to-slate-400 opacity-10 absolute rounded-2xl'></div>
@@ -68,7 +68,7 @@ export const ShoppingCart = () => {
 
                     <div className='flex gap-5 md:flex-row md:justify-between'>
                         <input className='border-2 border-teal-500 rounded-lg bg-transparent md:w-full' type="text" />
-                        <button className='bg-teal-600 text-white font-bold py-2 px-6 tracking-widest rounded-lg'>Apply</button>
+                        <button className='bg-teal-600 text-white font-bold py-2 px-3 tracking-widest rounded-lg'>Apply</button>
                     </div>
                 </div>
 
